a6e490da6f0b3e37cfb8fa2bed3ee841951d1af7
Tatana <burmatova@inbox.ru>
2020-07-22 20:04:28 +0300

    Mili.java added

---
+++ src/Mili.java
@@ -0,0 +1,7 @@
+public class Mili {
+    public static void main(String[] args) {
+        int ticketprices = 7000;
+        int totalmiles = ticketprices / 20;
+        System.out.println(totalmiles);
+    }
+}
